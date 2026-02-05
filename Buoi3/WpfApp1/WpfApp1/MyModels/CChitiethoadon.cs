2399065d6fdc7bfeea771747ad062f20bfb64f45
quangminh SV3 <quangminh_sv3@gmail.com>
2026-02-06 02:21:10 +0700

    update code

---
+++ Buoi3/WpfApp1/WpfApp1/MyModels/CChitiethoadon.cs
@@ -0,0 +1,17 @@
+﻿using WpfApp1.Models;
+
+namespace WpfApp1.MyModels
+{
+    public class CChitiethoadon
+    {
+        public string Sohd { get; set; }
+        public string Mahang { get; set; }
+        public double? Dongia { get; set; }
+        public int? Soluong { get; set; }
+        public string Tenhang { get { return MahangNavigation.Tenhang; } }
+        public string Dvt { get { return MahangNavigation.Dvt; } }
+        public double Thanhtien { get { return Soluong.Value * Dongia.Value; } }
+
+        public virtual Hanghoa MahangNavigation { get; set; }
+    }
+}
